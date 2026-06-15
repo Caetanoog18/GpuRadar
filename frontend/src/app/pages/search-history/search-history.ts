5832c2eccd7a2c25a62ebbb52a4f08b1f43464d1
Caetanoog18 <caetanoog@outlook.com>
2026-06-15 11:16:08 -0300

    Initialize Angular frontend structure

---
+++ frontend/src/app/pages/search-history/search-history.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-search-history',
+  imports: [],
+  templateUrl: './search-history.html',
+  styleUrl: './search-history.scss',
+})
+export class SearchHistory {}
