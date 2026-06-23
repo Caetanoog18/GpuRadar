2d031cf4529391c6d9cbcbfdf29440159526bd32
Gabriel Caetano de Oliveira <80604742+Caetanoog18@users.noreply.github.com>
2026-06-23 12:23:59 -0300

    Develop (#11)
    * render
    
    * test
    
    * test security

--- src/main/java/com/caetano/gpu_radar_api/config/OpenApiConfig.java
+++ src/main/java/com/caetano/gpu_radar_api/config/OpenApiConfig.java
@@ -1,13 +1,19 @@
 package com.caetano.gpu_radar_api.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class OpenApiConfig {
+
+    private static final String SECURITY_SCHEME_NAME = "bearerAuth";
+
     @Bean
     public OpenAPI gpuRadarOpenAPI() {
         return new OpenAPI()
@@ -23,6 +29,18 @@ public class OpenApiConfig {
                                         new Contact()
                                                 .name("Gabriel Caetano")
                                                 .email("caetanoog@outlook.com")
+                                )
+                )
+                .addSecurityItem(new SecurityRequirement().addList(SECURITY_SCHEME_NAME))
+                .components(
+                        new Components()
+                                .addSecuritySchemes(
+                                        SECURITY_SCHEME_NAME,
+                                        new SecurityScheme()
+                                                .name(SECURITY_SCHEME_NAME)
+                                                .type(SecurityScheme.Type.HTTP)
+                                                .scheme("bearer")
+                                                .bearerFormat("JWT")
                                 )
                 );
     }
